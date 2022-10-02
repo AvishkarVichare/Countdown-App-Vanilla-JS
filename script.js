37fac01c4f73b1104f21ccec82d76e55be1a05da
Avishkar Vichare <avie9500@gmail.com>
2022-10-02 13:22:32 +0530

    completed

--- script.js
+++ script.js
@@ -6,31 +6,43 @@ const resetBtn = document.querySelector('#reset-btn');
 let checkReset = false;
 let checkIn = false;
 // let count = 0;
-let id =  null
+let id = null;
 
 startBtn.addEventListener('click',()=>{
     let ourValue = cInput.value;
     let currCount = 0;  
     let count = 0;
     checkReset = false;
-    id = setInterval(countFun,1000);
-    if(checkIn){
-        console.log('clearing')
-        clearInterval(id);
 
-        checkIn = false;
+    if(id){
+        clearInterval(id);
+ 
     }
+
+    id = setInterval(countFun,1000);
 
   
 
     function countFun () {
-        checkIn = true;
+        if(checkReset){
+            countdown.innerHTML = 0;
+            clearInterval(id);
+            cInput.value = '';
+            return;
+        }
+
         currCount = ourValue - count;
         if(currCount === 0 ){
             clearInterval(id);
-           
         }
-        
+        if(currCount%2==0){
+            countdown.style.fontSize = '48px';
+        countdown.classList.add('animate-bounce')
+
+        }
+        else{
+            countdown.style.fontSize = '45px'
+        }
         countdown.innerHTML = currCount;
         count ++;
     }
